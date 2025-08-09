a6e27a3ff682f6ddbe17bc901a616e0f969f1d6d
mickeypause <serhii.pause@gmail.com>
2025-08-09 17:59:47 +0300

    Initial Commit

---
+++ clippy/clippyApp.swift
@@ -0,0 +1,17 @@
+//
+//  clippyApp.swift
+//  clippy
+//
+//  Created by Serhii Fozykosh on 09.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct clippyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
